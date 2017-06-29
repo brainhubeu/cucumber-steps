ce1cb00f6c87ecc5bdfdbf55abb04b968c211d9d
Marcin Dryka <marcin@brainhub.pl>
2017-06-29 16:15:35 +0200

    paths

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import givenSupportCode from 'supportCode/given';
-import whenSupportCode from 'supportCode/when';
-import thenSupportCode from 'supportCode/then';
+import givenSupportCode from './supportCode/given';
+import whenSupportCode from './supportCode/when';
+import thenSupportCode from './supportCode/then';
 
 export default ({ Given, When, Then }) => {
   givenSupportCode(Given);
